f5531d0963965d51e16a82e2ec09d7d1609230e2
Nathan Attoh <nnaattoh@st.ug.edu.gh>
2024-05-27 11:32:34 +0000

    README and screenshot

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# rn-assignment2-11357227
+
+# DCIT202 Assignment 2
+
+Created an app using the EXPO CLI Framework
+- Edited the home page / index page to "My name is YourName". 
+- Increased Font size to 24
+- Made "Yourname" Bold.
+
+## Authors
+
+- Nathan Attoh - 11357227
+
+![Alt text](./screenshot.png?raw=true "Image")
+
+
--- app-one/app/index.tsx
+++ app-one/app/index.tsx
@@ -7,9 +7,10 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#f8f7de" ,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{fontSize: 24}}>My Name is <Text style={{fontWeight: 'bold'}}> Nathan </Text></Text>
     </View>
   );
 }
